769a5bf2be5fc21ab8d4c736cc4551190623d099
karnikr <rokarnik@gmail.com>
2023-01-11 16:59:37 -0500

    Middle of Day 3 of Porject: Finishing the Login Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={[<Header/>,<Home/>]}/>
           <Route path='/checkout' element={[<Header/>,<Checkout/>]}/>
+          <Route path='/login' element={[<Login/>]}/>
         </Routes>
       </div>
     </Router>
